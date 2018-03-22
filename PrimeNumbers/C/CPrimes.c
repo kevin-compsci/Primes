0cb0a12176f2563543d89e32aaa0686318a973e0
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-03-21 22:03:27 -0500

    Add files via upload

---
+++ PrimeNumbers/C/CPrimes.c
@@ -0,0 +1,26 @@
+//Kevin Nguyen
+//Basic C (C files) program that outputs values up to a target value. The output values are prime numbers.
+
+/*
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "gcc *.c" to compile c files
+	3.) type in "a.out" to run out file
+*/
+
+
+//TODO: CREATE DYNAMIC ARRAY, array will hold a growing size of integers (primes). Need create, clean (free up memeory), and insert functions at the minimum.
+
+
+//include statements
+#include <stdio.h>
+
+//function definitions
+
+//main driver
+int main() {
+	//local declarations
+  	return 0;
+}
+
+//Functions implementations goes below
